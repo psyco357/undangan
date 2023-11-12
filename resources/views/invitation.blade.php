c4afefc62376bfa0ce2421adf829fa8aa0851eb3
psyco357 <Suryaputro45@gmail.com>
2023-11-12 12:26:46 +0700

    upload file

---
+++ resources/views/invitation.blade.php
@@ -0,0 +1,21 @@
+<style>
+    .bg {
+        background-color: lightblue;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+
+    .font-f {
+        font-family:  Fantasy;
+    }
+
+    .mt {
+        margin-top: 100px;
+    }
+</style>
+
+<body class="bg">
+    <h2 class="text-center mt font-f">Wedding Invitation</h2>
+</body>
